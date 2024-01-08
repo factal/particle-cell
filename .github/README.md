e9d9f4f2a73bd0a6c852ae7bbdec538dddd6cef7
factal <joji0425jp@gmail.com>
2024-01-08 23:18:17 +0900

    cleanup

--- .github/README.md
+++
@@ -1,3 +0,0 @@
-# particle cell
-# demo: https://factal.github.io/particle-cell/
-リサイズ処理がめんどくせーのでちゃんと書いてません
--- build.js
+++
@@ -1,5 +0,0 @@
-require('esbuild').build({
-  entryPoints: ['src/main.ts'],
-  bundle: true,
-  outfile: 'dist/main.js',
-}).catch( () => process.exit(1) )
